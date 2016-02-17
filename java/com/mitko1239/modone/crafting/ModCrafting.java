49168c1704f4eb2fb0941261af62dc4cc8c2b768
Dimitar Dimitrov <dimitarcho1@gmail.com>
2016-02-18 00:57:36 +0200

    Initial commit

---
+++ java/com/mitko1239/modone/crafting/ModCrafting.java
@@ -0,0 +1,21 @@
+package com.mitko1239.modone.crafting;
+
+import com.mitko1239.modone.blocks.ModBlocks;
+import com.mitko1239.modone.items.ModItems;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModCrafting {
+	public static void initCrafting() {
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.tutorialBlock), "##", "##", '#', ModItems.tutorialItem);
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.propertyBlock, 8, 0), "###", "#I#", "###", '#', Blocks.cobblestone, 'I', Items.quartz);
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.propertyBlock, 8, 1), "###", "#I#", "###", '#', Blocks.cobblestone, 'I', new ItemStack(Items.dye, 1, 0));
+		// GameRegistry.addShapelessRecipe(new
+		// ItemStack(ModBlocks.tutorialBlock), new Object[] {new
+		// ItemStack(ModItems.tutorialItem, 0, 4)});
+		GameRegistry.addSmelting(Items.diamond, new ItemStack(ModItems.tutorialItem), 1.0F);
+	}
+}
