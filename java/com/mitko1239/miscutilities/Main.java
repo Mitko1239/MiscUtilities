417fd0dbf0a85807ab053fbf85c4f69c73397b12
Mitko1239 <dimitarcho1@gmail.com>
2016-06-24 00:41:15 +0300

    Removed idea project files and underped mod version from main class

--- java/com/mitko1239/miscutilities/Main.java
+++ java/com/mitko1239/miscutilities/Main.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Main {
 	public static final String MODID = "miscutilities";
 	public static final String MODNAME = "Miscellaneous Utilities";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.4.0";
 	public static final ModCreativeTabs creativeTab = new ModCreativeTabs();
 	
 	@Instance
